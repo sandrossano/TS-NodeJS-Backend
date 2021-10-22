6a3652da742b58fd8d8d83640fed1cf4e2db31df
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-10-23 01:34:49 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const db = require("./config/db");
 const cors = require("cors");
 const crypto = require("crypto");
 const app = express();
-const PORT = process.env.PORT || 5000  # Fall back to port 5000 if process.env.PORT is not set
+const PORT = process.env.PORT || 5000 
 app.use(cors());
 app.use(express.json());
 
