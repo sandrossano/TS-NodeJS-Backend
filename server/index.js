51cd5e9c43842172075e6e24bf9f25eec6d64528
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-10-23 01:32:12 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const db = require("./config/db");
 const cors = require("cors");
 const crypto = require("crypto");
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 5000  # Fall back to port 5000 if process.env.PORT is not set
 app.use(cors());
 app.use(express.json());
 
@@ -86,6 +86,5 @@ app.delete("/api/delete/:id", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on` + PORT);
-});
+express()
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
