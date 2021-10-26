ecdc8392cc84f33557c78df4a6630abb91ed3571
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-10-26 20:38:24 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -73,8 +73,8 @@ app.get(
     const project = req.params.project;
     const task = req.params.task;
     db.query(
-      "INSERT INTO tt_log (id, user_id, group_id, org_id, date, duration, comment,client_id, project_id, task_id,created_by,status) " +
-        "SELECT MAX(id) + 1, ? , '1', '1', ? , ? , ? , ? , ? , ? , ? , '1' FROM tt_log;",
+      "INSERT INTO tt_log (id, user_id, group_id, org_id, date, duration, comment,client_id, project_id, task_id,created_by,status,billable) " +
+        "SELECT MAX(id) + 1, ? , '1', '1', ? , ? , ? , ? , ? , ? , ? , '1' , '1' FROM tt_log;",
       [user, date, duration, comment, client, project, task, user],
       (err, result) => {
         if (err) {
