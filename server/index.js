8b392f208e8ff8ff2b6cc74e89b1e51871c39027
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-10-27 23:51:37 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -222,6 +222,28 @@ app.get("/api/getevent/:id~:date", (req, res) => {
   });
 });
 
+const queryreport =
+  "SELECT tt_log.id,tt_log.date AS start,tt_log.date AS end," +
+  "tt_log.duration,tt_clients.name,tt_projects.name AS project," +
+  "tt_tasks.name AS task, tt_log.comment FROM tt_log INNER JOIN tt_users ON " +
+  "tt_log.user_id = tt_users.id INNER JOIN tt_clients ON " +
+  "tt_log.client_id = tt_clients.id INNER JOIN tt_projects " +
+  "ON tt_log.project_id = tt_projects.id INNER JOIN tt_tasks " +
+  "ON tt_log.task_id = tt_tasks.id WHERE tt_users.login = ? AND tt_log.date >= ? AND tt_log.date < ? AND tt_log.status = '1'" +
+  "ORDER BY tt_log.date DESC";
+// Route to get all posts
+app.get("/api/getreport/:id~:date~:enddate", (req, res) => {
+  const id = req.params.id;
+  const date = req.params.date;
+  const enddate = req.params.enddate;
+  db.query(queryreport, [id, date, enddate], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
 // Route to get one post
 app.get("/api/gettask", (req, res) => {
   db.query(
